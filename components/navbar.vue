aa97e4d58c875053ceeb6a8d3c397e472fdce92c
Shih-Pnag <leebond88@yahoo.com.tw>
2020-10-05 12:22:01 +0800

    add

---
+++ components/navbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <v-avatar class="mr-10" color="grey darken-1" size="32"></v-avatar>
+
+        <v-btn v-for="link in links" :key="link" text>
+          {{ link }}
+        </v-btn>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return{
+                links: [
+                    'Dashboard',
+                    'Messages',
+                    'Profile',
+                    'Updates',
+                    'User'
+                ],
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,12 +2,7 @@
   <v-app id="inspire">
     <v-app-bar app color="white" flat>
       <v-container class="py-0 fill-height">
-        <v-avatar class="mr-10" color="grey darken-1" size="32"></v-avatar>
-
-        <v-btn v-for="link in links" :key="link" text>
-          {{ link }}
-        </v-btn>
-        
+        <navbar/>
       </v-container>
     </v-app-bar>
 
@@ -15,27 +10,7 @@
       <v-container>
         <v-row>
           <v-col cols="2">
-            <v-sheet rounded="lg">
-              <v-list color="transparent">
-                <v-list-item v-for="n in 5" :key="n" link>
-                  <v-list-item-content>
-                    <v-list-item-title>
-                      List Item {{ n }}
-                    </v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
-
-                <v-divider class="my-2"></v-divider>
-
-                <v-list-item link color="grey lighten-4">
-                  <v-list-item-content>
-                    <v-list-item-title>
-                      Refresh
-                    </v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
-              </v-list>
-            </v-sheet>
+            <sidenav/>
           </v-col>
 
           <v-col>
@@ -51,13 +26,6 @@
 
 <script>
   export default {
-    data: () => ({
-      links: [
-        'Dashboard',
-        'Messages',
-        'Profile',
-        'Updates',
-      ],
-    }),
+    
   }
 </script>
