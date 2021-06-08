d2c0fdb829e7c50a14d50bdde101ec6f2b8a17a6
Shih-Pnag <leebond88@yahoo.com.tw>
2021-06-09 01:29:09 +0800

    add

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,6 +15,11 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1' },
       { hid: 'description', name: 'description', content: '' }
     ],
+    script:[
+      {
+        src: process.env.BASE_URL + '/qrcodejs-master/qrcode.min.js'
+      }
+    ],
     link: [
       // { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css',  integrity:'sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=', crossorigin: 'anonymous' }
---
+++ pages/test.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div ref="qrcode"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+          return {
+            qrcodeObj: {}
+          }
+        },
+        mounted() {
+            this.qrcodeObj = new QRCode(this.$refs.qrcode, {
+                  text: 'Always believe that good things are about to happen',    
+                  width: 200,
+                  height: 200,
+                  colorDark : '#0f0', // green
+                  colorLight : '#fff',
+                  correctLevel : QRCode.CorrectLevel.H
+            });
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
